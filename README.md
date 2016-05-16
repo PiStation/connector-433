8a540c6533d55135cbe294a229ab923e417dd7b7
Rob Quist <rob.quist@dukesoft.nl>
2016-05-16 18:52:56 +0200

    Working on connector

--- README.md
+++ README.md
@@ -1,12 +1,2 @@
 # connector-433
 General 433 connector to use the 433 protocol for a variety of devices
-
-# Requirements
-You will have to install wiringPi:
-
-```
-    cd /tmp
-    git clone git://git.drogon.net/wiringPi
-    cd wiringPi
-    ./build
-```
--- bin/src/433connector.cpp
+++ bin/src/433connector.cpp
@@ -14,30 +14,41 @@
 #include <mqueue.h>
 #include "RemoteSwitch.cpp"
 #include "NewRemoteTransmitter.cpp"
-#include "433connector.h"
 
 using namespace std;
 
 void usage() {
-    cout << "Usage - Todo.";
+    cout << "Usage: (Kaku only at the moment). \r\n <binary> <pinout> <repeat> <address> <unit> <on|off|0-15 (dim)> ";
 }
 
 void setColorReal(int color, int value) {
-
+    NewRemoteTransmitter TsetColor(ADDR_COLOR_SELECT, PIN_OUT, 260, REPEAT);
 }
 
 int main(int argc, char* argv[])
 {
     setbuf(stdout, NULL);
+    if (sizeof(argv) != 6) {
+        usage();
+        exit(1);
+    }
+    int pinout = atoi(argv[1]);
+    int repeat = atoi(argv[2]);
+    int address = atoi(argv[3]);
+    int unit = atoi(argv[4]);
+    int onoff = atoi(argv[5]);
+
     // load wiringPi
-    if(wiringPiSetup() == -1)
-    {
+    if(wiringPiSetup() == -1) {
             printf("WiringPi setup failed. Maybe you haven't installed it yet?");
             exit(1);
     }
+
     // setup pin and make it low (otherwise transmitter will block other 433 mhz transmitters like remotes)
-    pinMode(PIN_OUT, OUTPUT);
-    digitalWrite(PIN_OUT, LOW);
+    pinMode(pinout, OUTPUT);
+    digitalWrite(pinout, LOW);
+
+    /*
     if (strcmp(argv[1],"program") == 0) {
         cout << "Switching to program " << argv[2];
         //TselectProgram.sendUnit(atoi(argv[2]), true);
@@ -50,7 +61,7 @@ int main(int argc, char* argv[])
     } else {
         usage();
     }
-
+    */
     return 0;
 }
 
